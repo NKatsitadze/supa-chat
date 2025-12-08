5b4def683fbce2b58bfc95efde8366f408dff51a
Nika <nikakatsitadze98@gmail.com>
2025-12-08 22:41:37 +0400

    update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,13 @@
 "use client"
 
+import { useCurrentUser } from "@/services/supabase/hooks/useCurrentUser"
+
 export default function NavBar () {
-    
+    const {user, isLoading} = useCurrentUser()
+
+    return (
+        <div className="border-b bg-background h-header">
+            <nav></nav>
+        </div>
+    )
 }
